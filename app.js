55a3a9415cf0810a51c1d1459260b2efe5b11aa5
tombetthauser <tombetthauser@gmail.com>
2020-02-10 15:52:55 -0800

    Gets user login register current api working and runs create react app

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const express = require('express');
 const db = require('./config/keys').mongoURI;
-const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
-  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => {console.log('connected to mongo')})
-  .catch((err) => {console.log(err)})
-  
+.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => {console.log('connected to mongo')})
+.catch((err) => {console.log(err)})
+
+require("./models/User");
+const users = require('./routes/api/users');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.use('/api/users', users);
 
 app.get('/', (req, res) => {
