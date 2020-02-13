beabee8e4a1ebe135bacd78970ce47538a3fc2b3
tombetthauser <tombetthauser@gmail.com>
2020-02-13 15:29:04 -0800

    Cleans up application components and demo functionality before starting on Links and Routes

--- frontend/src/components/application/aside/aside.js
+++ frontend/src/components/application/aside/aside.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import "./aside.css";
 import SidebarProfile from "./sidebar_profile/sidebar_profile";
 
@@ -38,22 +39,10 @@ class LeftNavColumn extends React.Component {
       <div className="aside-div" title="application > aside.js">
         <SidebarProfile />
         <div className="aside-match-link-div">
-          <div
-            className="aside-match-link-image"
-            onClick={this.handleProfileButtonClick}
-            style={{
-              backgroundImage:
-                "url('https://s.yimg.com/uu/api/res/1.2/GBi4ioTdBU5pI_mj2qdoOA--~B/aD0xODAwO3c9MjcwMDtzbT0xO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/people_218/f4ad8855ecce83db4bad5aab2cc047e8')",
-              width: "50px",
-              height: "50px"
-            }}
+          <div className="aside-match-link-image" onClick={this.handleProfileButtonClick}
+            style={{ backgroundImage: "url('https://s.yimg.com/uu/api/res/1.2/GBi4ioTdBU5pI_mj2qdoOA--~B/aD0xODAwO3c9MjcwMDtzbT0xO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/people_218/f4ad8855ecce83db4bad5aab2cc047e8')" }}
           ></div>
-          <span
-            className="aside-match-link-text"
-            onClick={this.handleProfileButtonClick}
-          >
-            {tempUser.name}
-          </span>
+          <span className="aside-match-link-text" onClick={this.handleProfileButtonClick} >{tempUser.name}</span>
         </div>
       </div>
     );
