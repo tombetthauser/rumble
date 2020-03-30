85ca7fff8dd7085aa7a7fa2641fd747e521d270c
tombetthauser <tombetthauser@gmail.com>
2020-03-30 12:49:30 -0700

    Updates

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,24 +7,14 @@ import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import ApplicationComponent from "./application/application_component";
 
-// import EditFormContainer from "./profile/edit-profile-container";
-// import UserProfileContainer from "./profile/user-profile-container";
-// import MeetingsContainer from "./meetings/meetings-container";
-// import ConversationShow from "./chat/conversation_show";
-
 const App = () => (
   <div>
     <Switch>
       <ProtectedRoute path="/app" component={ApplicationComponent} />
       <AuthRoute exact path="/" component={SplashPageComponent} />
 
-
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-
-       <ProtectedRoute exact path="/meeting" component={MeetingsContainer}/>
-       <ProtectedRoute exact path='/user/:userId' component={UserProfileContainer}/>
-      <ProtectedRoute exact path='/edit/:userId' component={EditFormContainer}/> 
     </Switch>
   </div>
 );
