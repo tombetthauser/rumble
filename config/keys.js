7801446f1ed5f1ddd51caf3f01853ecaeeb73854
John Enriquez <john.enriquez@gmail.com>
2020-03-30 11:07:41 -0700

    removes conflict stuff from keys

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod.js");
-} else {
-  module.exports = require("./keys_dev");
-=======
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./keys_prod');
 } else {
     module.exports = require('./keys_dev');
->>>>>>> 034770a40465dd1e801a52c5b9f40a82ecd027b8
 }
